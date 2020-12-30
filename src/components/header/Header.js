1cbc88f074fca5edceda6e87fa8c4c42728a92cd
Maksym Pavliv <pavliv.maksym@gmail.com>
2020-12-30 14:12:24 +0200

    REACT Hm-9 with total price

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,23 @@
+import {useMemo} from "react";
 import {useSelector} from "react-redux";
 
 export default function Header() {
     const {wishList, cart} = useSelector(({wishList: {wishList}, cart: {cart}}) => ({wishList, cart}));
 
+    const wishListPrice = useMemo(() => {
+        return wishList.reduce((acc, el) => acc += el.price, 0);
+    }, [wishList]);
+
+    const cartPrice = useMemo(() => {
+        return cart.reduce((acc, el) => acc += el.price, 0);
+    }, [cart]);
+
     return (
         <header className={'d-flex justify-between p20 align-center'}>
             <h1>最高の店</h1>
             <div className={'d-flex'}>
-                <div className={'p20'}>Wish list: {wishList.length}</div>
-                <div className={'p20'}>Shopping cart: {cart.length}</div>
+                <div className={'p20'} title={'Total price: ' + wishListPrice + '$'}>Wish list: {wishList.length}</div>
+                <div className={'p20'} title={'Total price: ' + cartPrice + '$'}>Shopping cart: {cart.length}</div>
             </div>
         </header>
     );
