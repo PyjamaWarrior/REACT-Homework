c5a1e98bfc2b95941b70bcd1a6de7cb080a5e17b
freefireman <beluh.c.s@gmail.com>
2021-01-20 13:20:56 +0200

    little fix))

--- src/components/products-list/ProductsList.js
+++ src/components/products-list/ProductsList.js
@@ -2,8 +2,9 @@ import ProductItem from "../product-item/ProductItem";
 import {useDispatch, useSelector} from "react-redux";
 import {toggleItemInCart, toggleItemInWishList} from "../../redux/action-creators";
 
-export default function ProductsList({products}) {
-    const {wishList, cart} = useSelector(({wishList: {wishList}, cart: {cart}}) => ({wishList, cart}));
+export default function ProductsList() {
+    const {products,wishList, cart} = useSelector(({wishList: {wishList}, cart: {cart}, products:{products}}) =>
+        ({wishList, cart,products}));
     const dispatch = useDispatch();
 
     const onAddToWishList = (product) => {
